46a89604cbe58857bcef99cf13693bb98caaddbc
Lisa Yoder <lisa.alane.yoder@gmail.com>
2013-10-08 22:05:42 -0400

    restyle navigation & dropdown

--- includes/header.php
+++ includes/header.php
@@ -1,17 +1,22 @@
 <a href="/index.php"><img src="/images/logo.gif" width="270" height="99" alt="NorthStar Museums Logo" class="logo" /></a>
 <div id="navigation">
   <ul>
-   <li id="home"><a href="/index.php"></a></li>
-    <li id="about">About</li>
-      <ul class="dropdown">
+   <li id="home"><a href="/index.php">Home</a></li>
+    
+    <li id="about"><a href="#">About</a>
+      <ul>
         <li><a href="/about/philadelphia/index.php">Philadelphia</a></li>
         <li><a href="/about/newyork/index.php">New York</a></li>
       </ul>
-    <li id="case-studies">Case Studies</li>
-      <ul class="dropdown">
+    </li>
+    
+    <li id="case-studies"><a href="#">Case Studies</a>
+      <ul>
         <li><a href="/case-studies/philadelphia/index.php">Philadelphia</a></li>
         <li><a href="/case-studies/newyork/index.php">New York</a></li>
       </ul>
+    </li>
+    
     <li id="services"><a href="/services.php">Services</a></li>
     <li id="testimonials"><a href="/testimonials.php">Testimonials</a></li>
     <li id="news"><a href="/nsmnews/index.php">News</a></li>
